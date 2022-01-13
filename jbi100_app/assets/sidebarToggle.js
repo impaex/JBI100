e9b910dddc48ce0dfec939f21754797932070589
Impaex <max.den.oudsten@gmail.com>
2022-01-13 23:28:54 +0100

    Button toggle script

---
+++ jbi100_app/assets/sidebarToggle.js
@@ -0,0 +1,15 @@
+setTimeout(() => {
+
+let button = document.getElementById('toggleSide');
+button.addEventListener('click', e => {
+    let panel = document.getElementById('right-column');
+    if (panel.style.display == 'none') {
+        panel.style.display = 'block';
+    }
+    else {
+        panel.style.display = 'none';
+    }
+});
+
+}, 1000);
+
