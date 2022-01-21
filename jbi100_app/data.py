6751feea113056e8edcf96fff255cff56823eb82
Impaex <max.den.oudsten@gmail.com>
2022-01-21 19:59:26 +0100

    Added data-preloader

--- jbi100_app/data.py
+++ jbi100_app/data.py
@@ -1,10 +1,9 @@
-import plotly.express as px
+import pandas as pd
+import os
 
 
 def get_data():
     # Read data
-    df = px.data.iris()
-
-    # Any further data preprocessing can go her
-
-    return df
+    df = pd.read_csv(os.path.join(os.path.dirname(__file__), '../dataset/cleaned_data.csv'))
+    df1 = df[df["accident_year"] == 2020]
+    return df1
