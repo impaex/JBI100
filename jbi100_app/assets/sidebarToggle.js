51205436ac49f2f19ce843eb20c72a1792b090ee
Impaex <max.den.oudsten@gmail.com>
2022-02-03 02:54:48 +0100

    improved side bar toggle

--- jbi100_app/assets/sidebarToggle.js
+++ jbi100_app/assets/sidebarToggle.js
@@ -3,10 +3,13 @@ setTimeout(() => {
 let button = document.getElementById('toggleSide');
 button.addEventListener('click', e => {
     let panel = document.getElementById('right-column');
+    let map = document.getElementById('left-column');
     if (panel.style.display == 'none') {
+        map.className = "six columns";
         panel.style.display = 'block';
     }
     else {
+        map.className = "nine columns";
         panel.style.display = 'none';
     }
 });
